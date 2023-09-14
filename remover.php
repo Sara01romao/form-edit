73b15c9485afb4dd511bdff44082179be52f9b1b
Sara Romão <sara-romao@live.com>
2023-09-14 16:38:15 -0300

    feat: remover campo

--- remover.php
+++ remover.php
@@ -4,9 +4,26 @@ include './conexao.php';
 
 $id = $_GET['id'];
 
-$sql = "DELETE FROM `formcampos` WHERE id = $id";
 
-$deletar= mysqli_query($conexao, $sql);
+
+// $sql = "DELETE FROM `formcampos` WHERE id = $id";
+
+// $deletar= mysqli_query($conexao, $sql);
+
+
+// Exclua na tabela opcaocampo
+$sqlOpcaocampo = "DELETE FROM opcaocampo WHERE id_formcampo = $id";
+$deletarOpcaocampo = mysqli_query($conexao, $sqlOpcaocampo);
+
+
+
+// Exclua na tabela formcampos
+$sqlFormcampos = "DELETE FROM formcampos WHERE id = $id";
+$deletarFormcampos = mysqli_query($conexao, $sqlFormcampos);
+
+
+
+
 
 
 header("Location: ./index.php");
